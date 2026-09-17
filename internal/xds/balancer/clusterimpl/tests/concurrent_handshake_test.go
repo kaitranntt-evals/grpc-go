24ac0268cf7e2ed625c6565d54894b15e9df22e4
Tam Nhu Tran <kaitran.ntt@gmail.com>
2026-09-17 12:42:57 -0400

    fix(test): synchronize replacement applied before handshake release

--- internal/xds/balancer/clusterimpl/tests/concurrent_handshake_test.go
+++ internal/xds/balancer/clusterimpl/tests/concurrent_handshake_test.go
@@ -1,3 +1,21 @@
+/*
+ *
+ * Copyright 2024 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package clusterimpl_test
 
 import (
@@ -12,10 +30,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/tls/certprovider"
+	"google.golang.org/grpc/internal"
+	"google.golang.org/grpc/internal/stubserver"
 	"google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/internal/testutils/xds/e2e"
 	"google.golang.org/grpc/internal/xds/bootstrap"
+	"google.golang.org/grpc/internal/xds/xdsclient/xdsresource"
+	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/testdata"
 
 	v3clusterpb "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
@@ -125,6 +148,50 @@ func (b *calMinProviderBuilder) ParseConfig(any) (*certprovider.BuildableConfig,
 // TestSecurityConfigUpdate_ConcurrentHandshake checks that a client handshake
 // that started loading validation roots keeps its selected Cluster security
 // configuration usable after replacement.
+type calMinResolverBuilder struct {
+	resolver.Builder
+	replacementRoot string
+	applied         chan struct{}
+}
+
+func (b *calMinResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	return b.Builder.Build(target, &calMinClientConn{
+		ClientConn:      cc,
+		replacementRoot: b.replacementRoot,
+		applied:         b.applied,
+	}, opts)
+}
+
+type calMinClientConn struct {
+	resolver.ClientConn
+	replacementRoot string
+	applied         chan struct{}
+}
+
+func (cc *calMinClientConn) UpdateState(state resolver.State) error {
+	err := cc.ClientConn.UpdateState(state)
+	if err != nil || state.Attributes == nil {
+		return err
+	}
+	config := xdsresource.XDSConfigFromResolverState(state)
+	if config == nil {
+		return nil
+	}
+	for _, result := range config.Clusters {
+		if result == nil || result.Err != nil || result.Config.Cluster == nil || result.Config.Cluster.SecurityCfg == nil {
+			continue
+		}
+		if result.Config.Cluster.SecurityCfg.RootInstanceName == cc.replacementRoot {
+			select {
+			case cc.applied <- struct{}{}:
+			default:
+			}
+			break
+		}
+	}
+	return nil
+}
+
 func TestSecurityConfigUpdate_ConcurrentHandshake(t *testing.T) {
 	const (
 		instanceA = "handshake-lifetime-root-a"
@@ -183,7 +250,25 @@ func TestSecurityConfigUpdate_ConcurrentHandshake(t *testing.T) {
 		t.Fatalf("Failed to create bootstrap configuration: %v", err)
 	}
 
-	cc, serverAddress := setupForSecurityTests(t, bootstrapContents, xdsClientCredsWithInsecureFallback(t), tlsServerCreds(t))
+	r, err := internal.NewXDSResolverWithConfigForTesting.(func([]byte) (resolver.Builder, error))(bootstrapContents)
+	if err != nil {
+		t.Fatalf("Failed to create xDS resolver for testing: %v", err)
+	}
+	appliedCh := make(chan struct{}, 1)
+	r = &calMinResolverBuilder{
+		Builder:         r,
+		replacementRoot: instanceB,
+		applied:         appliedCh,
+	}
+	cc, err := grpc.NewClient(r.Scheme()+":///"+target, grpc.WithTransportCredentials(xdsClientCredsWithInsecureFallback(t)), grpc.WithResolvers(r))
+	if err != nil {
+		t.Fatalf("grpc.NewClient() failed: %v", err)
+	}
+	cc.Connect()
+	t.Cleanup(func() { cc.Close() })
+	server := stubserver.StartTestService(t, nil, grpc.Creds(tlsServerCreds(t)))
+	t.Cleanup(server.Stop)
+	serverAddress := server.Address
 	resources := e2e.DefaultClientResources(e2e.ResourceParams{
 		DialTarget: target,
 		NodeID:     nodeID,
@@ -222,7 +307,7 @@ func TestSecurityConfigUpdate_ConcurrentHandshake(t *testing.T) {
 	replacementApplied := false
 	for i := 0; i < 1000 && !replacementApplied; i++ {
 		select {
-		case <-builtB:
+		case <-appliedCh:
 			replacementApplied = true
 		default:
 			time.Sleep(time.Millisecond)
